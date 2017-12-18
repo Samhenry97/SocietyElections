c813c6558333e3392de6a9bbd06ae9d96619ea5a
cshean <cartershean2@gmail.com>
2017-12-18 02:01:01 -0500

    Added polling directory, hooked app up to postgresql database, created simple http response, moved static directory

--- elections/settings.py
+++ elections/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'elections.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'election',
+        'USER': 'postgres',
+        'PASSWORD': 'noneofyourbusiness',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
