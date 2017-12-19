4ecbc789885c6e62fd7c70bac452840b167fab67
cshean <cartershean2@gmail.com>
2017-12-19 03:07:11 -0500

    Added sample person table to \polls\model.py and updated \elections\settings.py accordingly

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	major = models.CharField(max_length=50)
+	rank = models.CharField(max_length=10)
+	position = models.CharField(max_length=20)
