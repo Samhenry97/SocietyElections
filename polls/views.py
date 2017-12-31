93c9c6e4dea2e6afde8f12a3145f0768c328e879
cshean <cartershean2@gmail.com>
2017-12-31 18:42:35 -0500

    added views/pages to the webpage

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+	return HttpResponse("""You're looking at question {}.""".format(question_id))
+
+def results(request, question_id):
+	return HttpResponse("""You're looking at the results of question {}.""".format(question_id))
+
+def vote(request, question_id):
+	return HttpResponse("""You're voting on question {}.""".format(question_id))
